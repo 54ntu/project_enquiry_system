1db1feed021634345cd510251b40a9e26429e6ae
54ntu <chaudharysantaram8@gmail.com>
2023-06-15 08:32:00 +0545

    static file is updated

--- project_student_enquiry/app_students/urls.py
+++ project_student_enquiry/app_students/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('create/',views.student_create,name='student-create'),
@@ -11,4 +13,5 @@ urlpatterns = [
 
 
 
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- project_student_enquiry/project_student_enquiry/urls.py
+++ project_student_enquiry/project_student_enquiry/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app_courses/',include('app_courses.urls')),
     path('app_students/',include('app_students.urls')),
     path('',include('app_accounts.urls'))
-]
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) #this line of code is for using the static url
+
