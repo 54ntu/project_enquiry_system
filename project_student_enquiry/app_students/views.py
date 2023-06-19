13005bab6c7dc00cf4468d89a04e27967b259233
54ntu <chaudharysantaram8@gmail.com>
2023-06-19 23:43:01 +0545

    user is added

--- project_student_enquiry/app_students/views.py
+++ project_student_enquiry/app_students/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from app_students.models import studentModel
+from app_students.models import studentModel,courseModel
 from app_students.forms import StudentCreateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -9,13 +10,27 @@ from django.contrib.auth.decorators import login_required
 def student_create(request):
     form= StudentCreateForm()
     context={"form": form}
-
-
     if request.method == "POST":
-        std = StudentCreateForm(request.POST, request.FILES) ##### here we use  object of form
-        if std.is_valid():
-            std.save()
-            return redirect("student-index")
+        # user= user.objects.get(id=request.user.id)
+
+        # std = StudentCreateForm(request.POST, request.FILES) ##### here we use  object of form
+        course =courseModel.objects.get(id=request.POST.get('course'))
+        std_odj = studentModel()
+        std_odj.first_name = request.POST.get('first_name')
+        std_odj.middle_name = request.POST.get('middle_name')
+        std_odj.last_name = request.POST.get('last_name')
+        std_odj.email =request.POST.get('email')
+        std_odj.contact = request.POST.get('contact')
+        std_odj.address= request.POST.get('address')
+        std_odj.created_at=request.POST.get('created_at')
+        std_odj.current_degree=request.POST.get('current_degree')
+        std_odj.course=course
+        std_odj.profile_img = request.POST.get('profile_img')
+        std_odj.user= request.user
+        std_odj.save()
+        # if std.is_valid():
+        #     std.save()
+        #     return redirect("student-index")
         return redirect("student-create")
     return render(request,"students/create.html",context)
 
@@ -47,7 +62,7 @@ def student_show(request,id):
 def student_update(request):
     if request.method == "POST":
         instance = studentModel.objects.get(id=request.POST.get('id'))
-        data =StudentCreateForm(data=request.POST,instance=instance)
+        data =StudentCreateForm(data=request.POST,instance=instance,files=request.FILES)
         if data.is_valid:
             data.save()
             return redirect('student-index')
